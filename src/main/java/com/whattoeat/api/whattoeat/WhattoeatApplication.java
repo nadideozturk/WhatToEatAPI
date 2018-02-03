19ed7984a47317dc51b1887bf453cafbb635915d
Nadide Ozturk <nadidebaca@gmail.com>
2018-02-03 12:05:00 -0800

    Backend initial commit

---
+++ src/main/java/com/whattoeat/api/whattoeat/WhattoeatApplication.java
@@ -0,0 +1,12 @@
+package com.whattoeat.api.whattoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattoeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhattoeatApplication.class, args);
+	}
+}
